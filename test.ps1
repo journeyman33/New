8b6fa25b6219c1adf9944faea596d4d708b539fe
Charles Vosloo <voslooc@gmail.com>
2019-01-26 14:16:45 +0200

    hello world script

--- test.ps1
+++ test.ps1
@@ -1,1 +1,5 @@
 write-output "hello world"
+
+write-host "Tell me you name!" -ForegroundColor Green
+$x = read-host "So, Your name is ?" # write-host is to screen only and not pipeline!
+write-host "hello", $x
